5a8c9afc48e42010439e9a6961991fe37d3a04b8
vdarji <vdarji@ggc.edu>
2016-08-19 16:18:20 -0400

    Update 01-read_input.sh

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -3,7 +3,7 @@
 
 echo "Hello! What is your name"
 read name
-echo "Welcome, $name"
+echo "Welcome, Vimal Darji"
 
 # single quotes prevent the expansion of the variable
 echo 'Your name was stored in $name'
